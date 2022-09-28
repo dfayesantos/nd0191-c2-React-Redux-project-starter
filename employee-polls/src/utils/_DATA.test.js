11e9c274e274b38e79016f65b0e09a67494f860d
Santos, Dianne (ds611v) <dfayesantos@gmail.com>
2022-09-28 11:51:02 -0500

    Update _DATA.test.js

--- employee-polls/src/utils/_DATA.test.js
+++ employee-polls/src/utils/_DATA.test.js
@@ -1,21 +1,58 @@
-const {_saveQuestionAnswer} = require("./_DATA");
+const {_saveQuestionAnswer, _saveQuestion} = require("./_DATA");
+
+describe("_saveQuestion", () => {
+    it("expected to return true", async () => {
+        const optionOneText = "walk to school"
+        const optionTwoText = "bike to school"
+        const author = 'zoshikanlu'
+        
+        const res = await _saveQuestion({
+            optionOneText: optionOneText,
+            optionTwoText: optionTwoText,
+            author: author,
+        });
+        const question = {
+            "id": res.id,
+            "timestamp": res.timestamp,
+            "author": author,
+            "optionOne": {
+              votes: [],
+              text: optionOneText,
+            },
+            "optionTwo": {
+              votes: [],
+              text: optionTwoText,
+            },
+        }
+        expect(res).toEqual(question)
+    });
+
+    it("expected to return error and reject", async () => {
+        const res = await _saveQuestion({
+            optionOneText: undefined,
+            optionTwoText: 'bike to school',
+            author: 'zoshikanlu',
+        }).catch(e => e);
+        expect(res).toBe("Please provide optionOneText, optionTwoText, and author");
+    });
+});
 
 describe("_saveQuestionAnswer", () => {
     it("expected to return true", async () => {
-        const response = await _saveQuestionAnswer({
+        const res = await _saveQuestionAnswer({
             authedUser: "mtsamis",
             qid: "vthrdm985a262al8qx3do",
             answer: "optionTwo"
         });
-        expect(response).toBeTruthy();
+        expect(res).toBeTruthy();
     });
 
     it("expected to return error and reject", async () => {
-        const response = await _saveQuestionAnswer({
+        const res = await _saveQuestionAnswer({
             authedUser: "zoshikanlu",
             qid: undefined,
             answer: "optionTwo"
         }).catch(e => e);
-        expect(response).toBe("Please provide authedUser, qid, and answer");
+        expect(res).toBe("Please provide authedUser, qid, and answer");
     });
 });
